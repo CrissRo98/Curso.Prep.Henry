9cc635a661e3a4370e8fc9671a415251f7e65f27
Crobles23 <CristianRobles_1998@outlook.com>
2022-06-28 17:06:44 -0500

    unidad terminada, arigato gozaimasu

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -91,6 +91,8 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var less = Math.max(...numeros);
+  return less;
 }
 
 
@@ -98,13 +100,18 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var Yud = 1;
+  if(arguments.length === 0){return 0;}
+  else if(arguments.length === 1){return arguments[0];}
+  else{for(var i = 0; i < arguments.length; i++){Yud = Yud * arguments[i]}} return Yud;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var vib = 0;
+for(var i = 0; i < arreglo.length; i++){if(arreglo[i] > 18){  vib += 1}}return vib;
 }
 
 
@@ -113,7 +120,7 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia === 1 || numeroDeDia === 7){return "Es fin de semana";} else{return "Es dia Laboral";}
 } 
 
 
@@ -121,7 +128,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var lessString = n.toString();
+  if(lessString[0] === "9"){return true;}
+  return false;
 }
 
 
@@ -129,7 +138,7 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for(var i = 0; i < arreglo.length -1; i++){if(arreglo[i] !== arreglo[i+1]){return false;}} return true;
 } 
 
 
@@ -138,6 +147,10 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var tazakentucky = [];
+  for(var i = 0; i < array.length; i++){if(array[i] === "Enero" || array[i] === "Marzo" ||array[i] === "Noviembre"){tazakentucky.push(array[i])}
+else if(tazakentucky.length === 3){return tazakentucky;}
+} return "No se encontraron los meses pedidos";
 }
 
 
@@ -145,6 +158,8 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+var mochila = [];
+for(var i = 0; i < array.length; i++){if(array[i] > 100){mochila.push(array[i])}}return mochila;
 }
 
 
@@ -156,6 +171,11 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var suma = numero;
+  var caja = [];
+  for(var i = 0; i < 10 ; i++){suma = suma + 2; if(suma === i){break;} else{caja.push(suma);}}
+  if(caja.length < 10){return "Se interrumpió la ejecución";} else if(caja.length === 10){return caja;}
+
 }
 
 
@@ -166,6 +186,9 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var compostable = [];
+  var RIteracion = numero;
+  for(var i = 0; i < 10; i++){if(i === 5){continue;}else {RIteracion = RIteracion + 2; compostable.push(RIteracion); }} return compostable;
 }
 
 
