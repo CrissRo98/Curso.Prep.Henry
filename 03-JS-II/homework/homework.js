6f28f02d8f78972efa63930f88e8a444e33420da
Crobles23 <CristianRobles_1998@outlook.com>
2022-08-26 20:42:45 -0500

    Objeto logrado el 20 de agosto uu

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,19 +5,19 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
-  if(x > y || x == y){
+  if(x > y){
     return x;
   }
-  else if(y > x || y == x){
+  else if(y > x){
     return y;
-  }
+  }else{return x||y;}
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-  if (edad == 18 || edad > 18){
+  if (edad >= 18){
     return "Allowed";
   }
   else if(edad < 18){
@@ -31,8 +31,8 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-  if (status == 1){return "Online";}
-  else if(status == 2){return "Away";}
+  if (status === 1){return "Online";}
+  else if(status === 2){return "Away";}
 return "Offline"
 }
 
---
+++ 05-JS-IV/alb.js
@@ -0,0 +1,35 @@
+ var Less = [{
+     Nombre: "Leslie",
+     Apellido: "Medina",
+     Trabaja: true,
+     Estudia: true,
+     PhoneNumber: 958367243,
+     DNI: 72314598,
+     Saludar: function(){console.log("Hola, " + this.Nombre + " " + this.Apellido)
+     return "nn"},
+     Hobbie: "Llegar tarde a cualquier lugar",
+     Gustan: ["ojos", "ocurrencias", "tenasidad", "resiliencia", "nobleza"],
+     Amigas: [{nombre: "Milito"}, {nombre:"Elito"}, {nombre:"Marito"},]
+},
+{
+    Test: "Blanca como la nieve",
+    Ojitos: "Café",
+    Nerviosa: "Colorada",
+    Laboral: "Trabaja muy duro",
+    Graduación: ["Vestido negro", "Casaca negra", "Tacos dorados", "Eres, hermosísima"],
+    Academicamente: {
+        Carrera: "Economía",
+        Uni: "U del Pacífico",
+        Distrito: "Jesús María",
+        Despierta: "5 a.m."
+    }
+}
+]
+
+var producto = {
+    aaaaa: 1111,
+    bbbbb: 2222,
+    ccccc: 3333,
+}
+
+
